7f1db4ca85789d0dacdc7ac91f23bb1c70cd71f3
Panics78390 <51930667+Panics78390@users.noreply.github.com>
2026-03-18 14:00:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='monark-v15';
+const CACHE='monark-v16';
 const ASSETS=['./index.html','./manifest.json','./icon-192.png','./icon-512.png','https://fonts.googleapis.com/css2?family=Syne:wght@400;500;600;700;800&family=JetBrains+Mono:wght@400;500;600&display=swap','https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{}))),self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))),self.clients.claim()});
